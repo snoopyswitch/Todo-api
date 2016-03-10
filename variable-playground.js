9ceb54d1840758af1f86c8bf32324e0e046bdb2e
Mladen Vidovic <snoopyswitch@gmail.com>
2016-03-10 12:38:41 +0100

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+//     name: 'Mladen',
+//     age: 21
+// };
+//
+// function updatePerson (obj) {
+//     // obj = {
+//     //     name: 'Mladen',
+//     //     age: 32
+//     // };
+//     obj.age = 32;
+// }
+//
+// updatePerson(person);
+// console.log(person);
+
+
+var grades = [15, 37];
+
+function addGrades (arr) {
+    arr.push(44);
+    //debugger;
+
+    //arr = [15, 37, 44];
+}
+
+addGrades(grades);
+console.log(grades);
