220c240f36b77e7c14fa52dde779ff14ca175a94
Mladen Vidovic <snoopyswitch@gmail.com>
2016-03-10 23:20:12 +0100

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -52,20 +53,26 @@ app.get('/todos/:id', function(req, res){
 app.post('/todos', function(req, res){
     var body = _.pick(req.body, 'description', 'completed');
 
-    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-        return res.status(400).send();
-    }
+    db.todo.create(body).then(function(todo){
+        res.json(todo.toJSON());
+    }, function (e){
+        res.status(400).json(e);
+    });
 
-    body.description = body.description.trim();
-
-
-    body.id = todoNextId;
-
-    todoNextId++;
-
-    todos.push(body);
-
-    res.json(body);
+    // if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    //     return res.status(400).send();
+    // }
+    //
+    // body.description = body.description.trim();
+    //
+    //
+    // body.id = todoNextId;
+    //
+    // todoNextId++;
+    //
+    // todos.push(body);
+    //
+    // res.json(body);
 });
 
 
@@ -113,8 +120,8 @@ app.put('/todos/:id', function(req, res){
 });
 
 
-
-
-app.listen(PORT, function(){
-    console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then(function (){
+    app.listen(PORT, function(){
+        console.log('Express listening on port ' + PORT + '!');
+    });
 });
